ce3449eeda07caf82cefea74ef735923efe91ae1
Mario Mancilla <mariormt17@gmail.com>
2021-07-21 16:58:27 -0500

    Add styles to the Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import conferenceLogo from '../images/badge-header.svg';
+import './styles/Badge.css'
 
 class Badge extends React.Component {
   render () {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={conferenceLogo} alt="conference-logo" />
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://static.platzi.com/media/avatars/avatars/mariormt17_16089d54-49bc-4747-a459-c23cf4b26b05.jpg"
+            alt="avatar"
+          />
           <h1>Mario <br /> Mancilla</h1>
         </div>
-        <div>
-          <p>Ruby on Rails Developer</p>
-          <p>@MarioManTin</p>
+        <div className="Badge__section-info">
+          <h3>Ruby on Rails Developer</h3>
+          <div>@MarioManTin</div>
         </div>
-        <div>
+        <div className="Badge__footer">
           #PlatziConf
         </div>
       </div>
