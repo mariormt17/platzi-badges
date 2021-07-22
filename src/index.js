e46c74da03921329692928c696824db0917da0a0
Mario Mancilla <mariormt17@gmail.com>
2021-07-22 11:31:40 -0500

    Create a Badges page that includes the BadgeList component to show a list of badges

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import './global.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 import BadgeNew from './pages/BadgeNew.js';
+import Badges from './pages/Badges.js';
 
 ReactDOM.render (
   <React.StrictMode>
-    <BadgeNew />
+    <Badges />
   </React.StrictMode>,
   document.getElementById('app')
 );
