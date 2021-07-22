7578d6e9a0845b7697c597f63c2c0c7237dfe711
Mario Mancilla <mariormt17@gmail.com>
2021-07-22 17:31:44 -0500

    Create the page to catch the error 404 and add its styles

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/NotFound.css';
+import error404 from '../images/404-error.svg';
 
 function NotFound () {
   return (
-    <h1>Not Found</h1>
+    <div className="NotFound">
+      <div className="container NotFound__container">
+        <div className="NotFound__info">
+          <h1>Error 404</h1>
+          <h3>Page Not Found</h3>
+          <p>Something went wrong</p>
+          <Link className="btn btn-primary btn-back-home" to="/">
+            Back to Home
+          </Link>
+        </div>
+        <img src={error404} alt="error404" />
+      </div>
+    </div>
   );
 }
 
